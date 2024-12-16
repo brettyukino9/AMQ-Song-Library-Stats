1bf14d0beab72b3e2d9785f80c76a8dfd6163772
bpreier <bpreier@ncsu.edu>
2024-12-16 03:25:21 -0500

    Added guess rate, difficulty, and recent percentage

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Use [this](https://github.com/Nick-NCSU/AMQ-Extended-Song-List/blob/main/generator.user.js) script to get your song library as a json file.
 
-Then run amq_stats.py
+Rename this json file to 'amq_stats.json', then run amq_stats.py
--- amq_stats.py
+++ amq_stats.py
@@ -3,12 +3,14 @@ import pandas as pd
 with open('amq_stats.json', 'rb') as file:
     data = json.load(file)
 
-data_list = pd.DataFrame(columns=['song', 'artist', 'type', 'anime', 'plays', 'correct count', 'percentage'])
+data_list = pd.DataFrame(columns=['song', 'artist', 'diff', 'anime', 'plays', 'correct count', 'percentage', 'recent percentage'])
 list = []
 for songid in data:
     entry = data[songid]
     song = entry['name']
     artist = entry['artist']
+    diff = entry['globalPercent']
+    last_eight = entry['recentPercent']
     for animeid in entry['anime']:
         anime_id_names = entry['anime'][animeid]['names']
         anime = anime_id_names['EN'] if anime_id_names['EN'] else anime_id_names['JA']
@@ -20,10 +22,10 @@ for songid in data:
     wrong_count = 0 if entry['totalWrongCount'] is None else int(entry['totalWrongCount'])
     plays = correct_count + wrong_count
     percentage = 0 if plays == 0 else (correct_count / plays) * 100
-    print(song, artist, anime, plays, correct_count, percentage)
-    list.append([song, artist, anime, plays, correct_count, percentage])
+    print(song, artist, diff, anime, plays, correct_count, percentage, last_eight)
+    list.append([song, artist, diff, anime, plays, correct_count, percentage, last_eight])
 
-data_list = pd.DataFrame(list, columns=['song', 'artist', 'anime', 'plays', 'correct count', 'percentage'])
+data_list = pd.DataFrame(list, columns=['song', 'artist', 'diff','anime', 'plays', 'correct count', 'percentage', 'recent percentage'])
 data_list = data_list.sort_values(by='plays', ascending=False)
 data_list.to_csv('amq_stats.csv', index=False)
 
@@ -34,10 +36,29 @@ above_80 = len(data_list[data_list['percentage'] >= 80])
 unlearned = len(data_list[(data_list['percentage'] < 80) & (data_list['plays'] > 0)])
 unplayed = len(data_list[data_list['plays'] == 0])
 
+# alternative method using recent percentage
+# above_70 = len(data_list[data_list['recent percentage'] >= 70])
+# unlearned = len(data_list[~((data_list['plays'] == 0) & (data_list['recent percentage'] == 0)) & (data_list['recent percentage'] < 70)])
+# unplayed = len(data_list[(data_list['plays'] == 0) & (data_list['recent percentage'] == 0)])
+
+
 print(f'Total entries: {total_entries}')
+print(f"Guess rate: {data_list['correct count'].sum() / data_list['plays'].sum() * 100}%")
 print(f'Learned entries (>80%): {above_80 / total_entries * 100}%')
 print(f'Unlearned entries (<80%): {unlearned / total_entries * 100}%')
 print(f'Unplayed entries: {unplayed / total_entries * 100}%')
+
+
+print("\n")
+under_30_songs = data_list[data_list['diff'] < 30]
+print("under 30 guess rate %: ", under_30_songs['correct count'].sum() / under_30_songs['plays'].sum() * 100)
+print("under 30 songs learned %: ", len(under_30_songs[under_30_songs['percentage'] >= 80]) / len(under_30_songs) * 100)
+print("under 30 songs unlearned %: ", len(under_30_songs[(under_30_songs['percentage'] < 80) & (under_30_songs['plays'] > 0)]) / len(under_30_songs) * 100)
+print("under 30 songs unplayed %: ", len(under_30_songs[under_30_songs['plays'] == 0]) / len(under_30_songs) * 100)
+
+# # print the under 30 songs in order of plays
+# under_30_songs = under_30_songs.sort_values(by='plays', ascending=False)
+# under_30_songs.to_csv('amq_under_30.csv', index=False)
 
 openings = data_list[data_list['song'].str.contains('OP')]
 
@@ -45,7 +66,7 @@ openings = data_list[data_list['song'].str.contains('OP')]
 # group data_list by anime
 data_list_anime = data_list.groupby('anime').agg({'plays': 'sum', 'correct count': 'sum'}).reset_index()
 data_list_anime['percentage'] = (data_list_anime['correct count'] / data_list_anime['plays']) * 100
-data_list_anime = data_list_anime.sort_values(by='percentage', ascending=False)
+data_list_anime = data_list_anime.sort_values(by='plays', ascending=False)
 data_list_anime.to_csv('amq_stats_anime.csv', index=False)
 
 data_list_anime_unlearned = data_list_anime[data_list_anime['percentage'] < 60]
